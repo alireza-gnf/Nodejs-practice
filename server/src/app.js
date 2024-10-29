9136044f8770c4f62384f55fc026540bd02d198a
Alireza Ghazanfari <alirezagnf99@gmail.com>
2024-10-29 21:41:55 +0330

    Initial commit

---
+++ server/src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const planetsRouter = require("./routes/planets/planets.router");
+
+const app = express();
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  console.log(
+    `${req.method} ${req.baseUrl}${req.url} - ${Date.now() - start}ms`
+  );
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(express.json());
+
+app.use("/planets", planetsRouter);
+
+module.exports = app;
---
+++ server/src/server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const app = require("./app");
+
+const { loadPlanets } = require("./models/planets.model");
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+async function startServer(port) {
+  await loadPlanets();
+
+  server.listen(PORT, () => console.log(`Listening on port ${port}...`));
+}
+
+startServer(PORT);
