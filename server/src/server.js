94814ac5d5689c3f6251f97f0af9eb2f332424e3
Alireza Ghazanfari <alirezagnf99@gmail.com>
2024-11-09 22:40:04 +0330

    Setup mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,21 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
 const { loadPlanets } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL = "mongodb://127.0.0.1:27017/nasa";
 
 const server = http.createServer(app);
-
+mongoose.connection.once("connected", () => console.log("Mongoose connected"));
+mongoose.connection.on("error", (err) => console.error(err));
+mongoose.connection.once("disconnected", () =>
+  console.log("Mongoose disconnected")
+);
 async function startServer(port) {
+  await mongoose.connect(MONGO_URL);
   await loadPlanets();
 
   server.listen(PORT, () => console.log(`Listening on port ${port}...`));
